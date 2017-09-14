afe05d72407c76e4aade8672169fab9a6dce0fc7
Alejandro Sanabria <alesanabriav@gmail.com>
2017-09-14 14:47:43 -0500

    add documentación

--- functions.php
+++ functions.php
@@ -1,6 +1,27 @@
 <?php
 
+/*
+En ajax van todas las funciones que manejan el ajax enviado desde el front.
+Documentación:
+https://codex.wordpress.org/Plugin_API/Action_Reference/wp_ajax_(action)
+*/
 require 'ajax/index.php';
+
+/*
+en lib va todas las librerias que necesitamos crear.
+*/
 require 'lib/index.php';
+
+/*
+en options va todas las paginas de opciones.
+Documentación:
+https://codex.wordpress.org/Creating_Options_Pages
+*/
 require 'options/index.php';
+
+/*
+en shortcodes va todos los shortcodes.
+Documentación:
+https://codex.wordpress.org/Creating_Options_Pages
+*/
 require 'shortcodes/index.php';
